2b47edd5e7ce714753ac60f43837283518ce8e6e
kennyp2233 <kennyp41234@gmail.com>
2024-02-20 12:36:42 -0500

    modified: Ingreso

--- src/main/java/modelo/Ingreso.java
+++ src/main/java/modelo/Ingreso.java
@@ -10,7 +10,7 @@ public class Ingreso extends Movimiento implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Integer id;
 
 	@Column(name = "fecha", nullable = false)
 	private Date fecha;
