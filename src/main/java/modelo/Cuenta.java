364133033c814c48e18c8e8efbcd4ba6765dc2ee
V1LL4IN <135792941+V1LL4IN@users.noreply.github.com>
2024-02-20 12:35:34 -0500

    Cuenta
    cuenta

--- src/main/java/modelo/Cuenta.java
+++ src/main/java/modelo/Cuenta.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.*;
-@Entity( name="cuenta")
+@Entity(name="cuenta")
 public class Cuenta implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -14,7 +14,8 @@ public class Cuenta implements Serializable{
 	private int id;
 	@Column(name = "nombre")
 	private String nombre;
-	
+
+	@OneToMany(mappedBy = "cuenta", cascade = CascadeType.ALL, orphanRemoval = true)
 	private static List<Cuenta> cuentas = null;
 	
 	public Cuenta() {
@@ -24,6 +25,55 @@ public class Cuenta implements Serializable{
 		super();
 		this.id = id;
 		this.nombre = nombre;
+
 	}
+
+	public Cuenta(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public static List<Cuenta> getCuentas() {
+		if (cuentas == null) {
+			cuentas = new ArrayList<Cuenta>();
+		}
+		return cuentas;
+	}
+
+	public static void setCuentas(List<Cuenta> cuentas) {
+		Cuenta.cuentas = cuentas;
+	}
+
+	public void addCuenta(Cuenta cuenta) {
+		getCuentas().add(cuenta);
+	}
+
+	public void removeCuenta(Cuenta cuenta) {
+		getCuentas().remove(cuenta);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Cuenta [id=" + id + ", nombre=" + nombre + "]";
+	}
+
+
 	
 }
